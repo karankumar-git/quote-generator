b41ccc05a9b0816889f26971787fb9972b62e0df
Karan Kumar <74554025+karankumar-git@users.noreply.github.com>
2021-05-15 01:06:06 +0530

    commit index, script and style files

---
+++ script.js
@@ -0,0 +1,46 @@
+let apiquote = [];
+let container = document.getElementsByClassName('container')[0];
+let spinner = document.getElementsByClassName('spinner-border')[0];
+//loading
+function loading(){
+    container.hidden = true;
+    spinner.hidden = false;
+}
+function complete(){
+    container.hidden = false;
+    spinner.hidden = true;
+}
+function newquote(){
+    let quote = apiquote[Math.floor(Math.random()* apiquote.length)];
+    return quote;
+}
+async function getquote(){
+    loading();
+    let apiUrl = "https://type.fit/api/quotes";
+    try{
+        
+        let response = await fetch(apiUrl);
+        apiquote= await response.json();
+        var array = newquote();
+        document.getElementById("quoteSection").innerText = array.text;
+        document.getElementsByTagName('h4')[0].innerText = "- " + array.author;
+        
+    }
+    catch(error){
+        console.log(error);
+    }
+    complete();
+}
+getquote();
+
+
+// tweet quote
+
+function tweet(){
+    const quote = newquote().text;
+    const author = newquote().author;
+    const twitterUrl = `https://twitter.com/intent/tweet?text = ${quote} - ${author}`; 
+    window.open(twitterUrl,'_blank');
+}
+
+document.getElementsByClassName("Tweet")[0].onclick=function(){tweet()};
